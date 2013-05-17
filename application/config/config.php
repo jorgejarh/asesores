fcf93e86bcb0b5dbc6cf2951b97e3b52ff65e310
Jorge Rodriguez <jorgejarh@gmail.com>
2013-05-16 22:54:25 -0600

    config

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:8080/asesores/';
+$config['base_url']	= 'http://localhost:8080/asesoress/';
 
 /*
 |--------------------------------------------------------------------------
