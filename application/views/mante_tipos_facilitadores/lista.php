420b465df3ec6f5dad603aeca6b0de255b2c4088
Victor Ricardo Barrera Hernández <vrbh85@gmail.com>
2013-06-06 12:07:31 -0600

    mante_tipos_facilitadores Listo!

---
+++ application/views/mante_tipos_facilitadores/lista.php
@@ -0,0 +1,111 @@
+<div id="content_main" class="clearfix">
+  <div id="main_panel_container" class="left" style="width:900px;">
+    <div id="dashboard" style="width:100%;padding-bottom:50px;">
+      <h2 class="ico_mug">
+      	<table style="width:100%;">
+      		<tr>
+      			<td><?php echo $title;?></td>
+      			<td style="text-align:right;"><button onClick="nuevo_registro();">Nuevo</button></td>
+      		</tr>
+      	</table>
+      </h2>
+      <div class="" style="width:90%; margin:auto;">
+      
+        <?php
+if($listado)
+{
+	?>
+  <table id="example" class="display" >
+    <thead>
+      <tr>
+      	<th>Tipo de Facilitador</th>
+        <th>Editar</th>
+        <th>Eliminar</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+		foreach($listado as $valor)
+		{
+			?>
+      <tr class="gradeA">
+      	<td><?php echo $valor['nombre_tipo_facilitador'];?></td>
+       
+        <td align="center" class="datatable_icono"><a onClick="editar_registro(<?php echo $valor[$this->$model->id_tabla]; ?>);"><?php echo img('public/img/edit.png');?></a></td>
+        <td align="center" class="datatable_icono">
+        
+          <a onClick="eliminar_registro(<?php echo $valor[$this->$model->id_tabla]; ?>);" title="Clic para Eliminar"><?php echo img('public/img/cancel.png');?></a>
+          </td>
+      </tr>
+      <?php
+		}
+		?>
+    </tbody>
+  </table>
+  <?php
+}
+?>
+          
+      </div>
+    </div>
+    <!-- end #dashboard --> 
+  </div>
+  
+  <!-- end #sidebar --> 
+  
+</div>
+<script type="text/javascript">
+	$(document).ready(function() {
+	    $('#example').dataTable( {
+	        <?php echo config_lenguaje_tabla(); ?>
+	    } );
+	} );
+
+function nuevo_registro()
+{
+	$.ajax({
+		  url: "<?php echo site_url($this->nombre_controlador.'/nuevo');?>",
+		  type:"POST",
+		  success:function(data){
+
+		  	$.fancybox(data);
+		  }
+		  
+		});
+}
+
+function editar_registro(id)
+{
+	$.ajax({
+		  url: "<?php echo site_url($this->nombre_controlador.'/editar');?>/"+id,
+		  type:"POST",
+		  success:function(data){
+
+		  	$.fancybox(data);
+		  }
+		  
+		});
+}
+
+function eliminar_registro(id)
+{
+	if(!confirm('¿Seguro que desea eliminar?'))
+	{
+		return false;
+	}
+
+	$.ajax({
+		  url: "<?php echo site_url($this->nombre_controlador.'/eliminar');?>",
+		  type:"POST",
+		  data:{'id':id},
+		  success:function(data){
+		  	
+		  		location.reload();
+		
+		  }
+		  
+		});
+}
+
+</script>
+
