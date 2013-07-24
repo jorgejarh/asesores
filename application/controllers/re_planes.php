936a09d29127dd977f1a1352e6f398fd78b89ffd
Jorge Rodriguez <jorgejarh@gmail.com>
2013-07-23 20:40:25 -0600

    comienzo de los reportes

---
+++ application/controllers/re_planes.php
@@ -0,0 +1,42 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Re_planes extends CI_Controller {
+
+	public $datos_user=array();
+
+	public $carpeta_view="re_planes";
+
+	public $modelo_usar="re_planes_model";
+
+	public $nombre_controlador="re_planes";
+	
+	public $nombre_titulo="Reportes";
+	
+	public $campos=array();
+	
+	
+	
+	
+	function __construct()
+    {
+        parent::__construct();
+        $this->datos_user=comprobar_login();
+        $model=$this->modelo_usar;
+		$this->load->model($model);
+
+    }
+
+	public function index()
+	{
+		$model=$this->modelo_usar;
+		$data['title']=$this->nombre_titulo;
+		$data['template']="sistema";
+		$data['contenido']=$this->carpeta_view."/lista";
+		$data['listado']=$this->$model->obtener();
+		$data['model']=$model;
+		$this->load->view('template',$data);
+
+	}
+	
+	
+}
---
+++ application/views/re_planes/lista.php
@@ -0,0 +1,24 @@
+<div id="content_main" class="clearfix">
+  <div id="main_panel_container" class="left" style="width:900px;">
+    <div id="dashboard" style="width:100%;padding-bottom:50px;">
+      <h2 class="ico_mug">
+      	<table style="width:100%;">
+      		<tr>
+      			<td><?php echo $title;?></td>
+      			
+      		</tr>
+      	</table>
+      </h2>
+      <div class="" style="width:90%; margin:auto;">
+      
+      
+          
+      </div>
+    </div>
+    <!-- end #dashboard --> 
+  </div>
+  
+  <!-- end #sidebar --> 
+  
+</div>
+
