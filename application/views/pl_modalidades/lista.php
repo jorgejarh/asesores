c0f8a79560271228b743854407282a61cb27e291
Jorge Rodriguez <jorgejarh@gmail.com>
2014-04-02 04:43:11 -0600

    quitar docs modalidad

--- application/views/pl_modalidades/lista.php
+++ application/views/pl_modalidades/lista.php
@@ -29,7 +29,7 @@ if($listado)
         <th>Descripción</th>
         <th>Total</th>
         <th>Asignar</th>
-        <th>Agregar Documentos</th>
+        <!--<th>Agregar Documentos</th>-->
         <th>&nbsp;</th>
         <th>&nbsp;</th>
       </tr>
@@ -44,7 +44,7 @@ if($listado)
         <td><?php echo $valor['objetivo'];?></td>
         <td>$ <?php echo number_format( obtener_costo_plan_modalidad($valor[$this->$model->id_tabla]),2);?></td>
         <td align="center"	class="datatable_icono"><a href="<?php echo site_url('pl_capacitaciones/index/'.$valor[$this->$model->id_tabla]);?>" ><?php echo img('public/img/ico_settings.png');?></a></td>
-        <td align="center" class="datatable_icono"><a href="<?php echo site_url('mante_modalidades_docs/index/'.$valor[$this->$model->id_tabla]);?>" ><?php echo img('public/img/ico_posts.png');?></a></td>
+        <!--<td align="center" class="datatable_icono"><a href="<?php echo site_url('mante_modalidades_docs/index/'.$valor[$this->$model->id_tabla]);?>" ><?php echo img('public/img/ico_posts.png');?></a></td>-->
         <td align="center" class="datatable_icono"><a title="Editar" onClick="editar_registro(<?php echo $valor[$this->$model->id_tabla]; ?>);"><?php echo img('public/img/edit.png');?></a></td>
         <td align="center" class="datatable_icono">
         
